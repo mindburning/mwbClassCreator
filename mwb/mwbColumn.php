ea7486296861d82cfe2ffae79e47919393d1a18a
micha <micha@micha-desktop>
2014-06-02 22:51:06 +0200

    + singleton column hinzugefügt

--- mwb/mwbColumn.php
+++ mwb/mwbColumn.php
@@ -4,4 +4,27 @@
  * Description of mwbColumn
  */
 class mwbColumn {
+	protected static $instances = array();
+	protected static $instancesByID = array();
+	
+	/**
+	 * erzeugt neue Instanz
+	 * @param mixed $data
+	 * @return \mwbColumn erzeugt eine neue Spalte
+	 */
+	public static function getInstance($data) {
+		$inst = new mwbColumn();
+		if( $data instanceof SimpleXMLElement ){
+			mwbUtils::assign($inst, $data);
+		}else{
+			if(isset(self::$instancesByID[$data])){
+				return self::$instancesByID[$data];
+			}
+		}
+		self::$instances[] = &$inst;
+		if($inst->id != ''){
+			self::$instancesByID[$inst->id] = &$inst;
+		}
+		return $inst;
+	}
 }
