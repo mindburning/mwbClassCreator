63e9e97936eb0ab14f63766171a1fffc4a59f543
mindburning <mindburning@users.noreply.github.com>
2014-06-02 23:02:57 +0200

    + added indexcolumn class

--- mwb/mwbIndexColumn.php
+++ mwb/mwbIndexColumn.php
@@ -4,4 +4,27 @@
  * Description of mwbIndexColumn
  */
 class mwbIndexColumn {
+	protected static $instances = array();
+	protected static $instancesByID = array();
+	
+	/**
+	 * erzeugt neue Instanz
+	 * @param SimpleXMLElement $data
+	 * @return \mwbIndexColumn
+	 */
+	public static function getInstance($data) {
+		$inst = new mwbIndexColumn();
+		if( $data instanceof SimpleXMLElement ){
+			mwbUtils::assign($inst, $data);
+		}else{
+			if(isset(self::$instancesByID[$data])){
+				return self::$instancesByID[$data];
+			}
+		}
+		self::$instances[] = &$inst;
+		if($inst->id != ''){
+			self::$instancesByID[$inst->id] = &$inst;
+		}
+		return $inst;
+	}
 }
