7937e64f2176dbbb4e9bb10718f5eb11bb09b96e
mindburning <mindburning@users.noreply.github.com>
2014-06-02 23:33:08 +0200

    + added target class

--- buildModel.php
+++ buildModel.php
@@ -5,6 +5,6 @@ include_once 'mwb/mwbReader.php';
 $d = new mwbReader();
 $d->outputFolder = __DIR__ . "/model/";
 $d->modelPrefix = '';
-$d->renderFile('./model.mwb');
+$d->renderFile('./model.mwb', 'phpclass.php');
 
 echo "model created";
