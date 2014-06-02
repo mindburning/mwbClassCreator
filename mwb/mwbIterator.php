06fafc333bbbb898ec195d536ea924d643e9df44
micha <micha@micha-desktop>
2014-06-02 22:42:11 +0200

    + basic iterator kommentare

--- mwb/mwbIterator.php
+++ mwb/mwbIterator.php
@@ -7,10 +7,19 @@ include_once './mwbColumn.php';
 class mwbIterator {
 	public $_items = array();
 	
+	/**
+	 * neues element erzeugen
+	 * @return \mwbColumn
+	 */
 	public function getNewItem(){
 		return new mwbColumn();
 	}
 	
+	/**
+	 * magic get
+	 * @param string $name
+	 * @return \mwbColumn
+	 */
 	public function __get($name) {
 		if( isset( $this->_items[$name] ) ){
 			return $this->_items[$name];
@@ -18,6 +27,11 @@ class mwbIterator {
 		return $this->getNewItem();
 	}
 	
+	/**
+	 * magic set
+	 * @param string $name
+	 * @param \mwbColumn $value
+	 */
 	public function __set($name, $value) {
 		$this->_items[$name] = $value;
 		return $this;
