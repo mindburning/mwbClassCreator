030f9a307d771e2b158195b751fa276cb4977051
mindburning <mindburning@users.noreply.github.com>
2014-06-02 23:15:11 +0200

    + mwb zip datei öffnen

--- mwb/mwbReader.php
+++ mwb/mwbReader.php
@@ -18,9 +18,22 @@ class mwbReader{
 	protected $zip = null;
 	
 	/**
-	 * 
+	 * Table- zu Classname wrapper
+	 * @param string $tableName Tabellenname aus mwb Datei
+	 * @return string
+	 */
+	public function getClassNameByTableName( $tableName ){
+		return ucfirst( $tableName );
+	}
+	
+	/**
+	 * öffnet die Zip Datei und erzeugt das Klassenmodell
 	 * @param string $fn Pfad zur mwb Datei
 	 */
 	public function renderFile( $fn ){
+		$modelPrefix = $this->modelPrefix;
+		$this->zip = new ZipArchive($fn);
+		$this->zip->open($fn);
+		$model = simplexml_load_string( $d=$this->zip->getFromName('document.mwb.xml') );
 	}
 }
